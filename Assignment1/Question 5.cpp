11fb58ffe7dbe194c2b6745dd513b677e4839f38
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2023-02-06 14:52:59 +0530

    Update Question 5.cpp

--- Assignment1/Question 5.cpp
+++ Assignment1/Question 5.cpp
@@ -8,9 +8,7 @@ Add `int max_of_four(int a, int b, int c, int d)` here.
 int max_of_four(int a, int b, int c ,int d)
 {
     if(a>b && a>c && a>d)
-    {
-        return a;
-    }
+    return a;
     else if (b>c && b>d)
     return b;
     else if (c>d)
